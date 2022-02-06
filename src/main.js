585479615fabb177084ab5a0971a044dbb759e8e
diongzy <yuii.yuii265@gmail.com>
2022-02-06 22:51:44 +0800

    feat: add an array and forEach method

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
 import { movePuzzle, resetTiles, goalState } from './puzzle.js';
 import BreadthFirst from './algorithms/breadth-first.js';
+const puzzleStates = [
+    [4, 8, 0, 7, 3, 2, 6, 5, 1],
+    [2, 6, 8, 7, 1, 0, 5, 3, 4],
+    [4, 8, 6, 7, 1, 2, 5, 3, 0],
+    [4, 3, 8, 5, 7, 2, 1, 0, 6],
+    [1, 7, 8, 4, 6, 2, 5, 3, 0],
+    [0, 7, 1, 2, 4, 5, 8, 3, 6],
+    [5, 4, 7, 0, 6, 3, 2, 1, 8],
+    [2, 5, 8, 4, 1, 3, 6, 0, 7],
+    [4, 6, 1, 8, 3, 2, 5, 7, 0],
+    [8, 0, 6, 1, 7, 4, 2, 5, 3]
+]
 
 document.addEventListener('keydown', (e) => {
     if (!e.key.includes('Arrow')) {
@@ -15,4 +27,4 @@ document.querySelectorAll('.control-buttons button').forEach(button => {
 });
 
 resetTiles();
-new BreadthFirst().time([3, 2, 5, 1, 8, 7, 0, 4, 6]);
+puzzleStates.forEach(state => new BreadthFirst().time(state))
