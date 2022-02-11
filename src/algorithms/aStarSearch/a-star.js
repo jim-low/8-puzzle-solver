954396fc22df8df508f882cc9c32dd3afd773168
jim-low <robil12380@gmail.com>
2022-02-11 14:06:54 +0800

    feat: implement A* Search roughly

--- src/algorithms/aStarSearch/a-star.js
+++ src/algorithms/aStarSearch/a-star.js
@@ -2,43 +2,33 @@ import Board from "./board.js";
 import PriorityQueue from "./priority-queue.js";
 
 export default class AStar {
-    static search(root, goal) {
-        // A* Search Algorithm
-        // 1.  Initialize the open list
-        // 2.  Initialize the closed list
+    static async search(root, goal) {
+        const openList = new PriorityQueue();
 
-        // put the starting node on the open list (you can leave its f at zero)
+        const closedList = new PriorityQueue();
 
-        // 3.  while the open list is not empty
-        //     a) find the node with the least f on the open list, call it "q"
+        openList.enqueue(new Board(root, 0));
 
-        //     b) pop q off the open list
+        while (openList.size() !== 0) {
+            const q = openList.dequeue();
 
-        //     c) generate q's 8 successors and set their parents to q
+            const successors = q.getSuccessors();
 
-        //     d) for each successor
-        //         i) if successor is the goal, stop search
+            successors.forEach(async (successor) => {
+                if (Board.isEqual(successor, goal)) {
+                    return Board.constructPath(successor);
+                }
 
-        //         ii) else, compute both g and h for successor successor.g = q.g + distance between successor and q
-        //           successor.h = distance from goal to
-        //           successor (This can be done using many
-        //           ways, we will discuss three heuristics-
-        //           Manhattan, Diagonal and Euclidean
-        //           Heuristics)
+                const res = !openList.checkSuccessorWithLowerPriority(successor) && !closedList.checkSuccessorWithLowerPriority(successor);
+                if (res) {
+                    openList.enqueue(successor);
+                }
+            });
 
-        //           successor.f = successor.g + successor.h
+            closedList.enqueue(q);
+        }
 
-        //         iii) if a node with the same position as
-        //             successor is in the OPEN list which has a
-        //            lower f than successor, skip this successor
-
-        //         iV) if a node with the same position as
-        //             successor  is in the CLOSED list which has
-        //             a lower f than successor, skip this successor
-        //             otherwise, add  the node to the open list
-        //      end (for loop)
-
-        //     e) push q on the closed list
-        //     end (while loop)
+        // no solution
+        return [];
     }
 }
