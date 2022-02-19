ab91a93f15a75d80015bbc8a559d6fde18934a2c
jim-low <robil12380@gmail.com>
2022-02-19 10:09:06 +0800

    feat: add time indicator to BFS and A* Search

--- src/main.js
+++ src/main.js
@@ -5,13 +5,17 @@ import AStar from './algorithms/aStarSearch/a-star.js';
 let path = [];
 let memory = 0;
 puzzleStates.forEach(puzz => {
+    console.time('Breadth First Search');
     path = new BreadthFirst().time(puzz);
+    console.timeEnd('Breadth First Search');
     console.log(`Memory Used: ${window.performance.memory.usedJSHeapSize} byte`);
     memory += window.performance.memory.usedJSHeapSize
     console.log(path);
 
 
+    console.time('A* Search');
     path = AStar.search(puzz, goalState);
+    console.timeEnd('A* Search');
     console.log(`Memory Used: ${window.performance.memory.usedJSHeapSize} byte`);
     memory += window.performance.memory.usedJSHeapSize
     console.log(path);
