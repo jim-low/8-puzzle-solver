d68373da47655b52fc83c28c9efac9336daf1019
jim-low <robil12380@gmail.com>
2022-02-19 10:07:24 +0800

    feat: add memory indicator to A* Search

--- src/main.js
+++ src/main.js
@@ -3,12 +3,16 @@ import BreadthFirst from './algorithms/breadth-first.js';
 import AStar from './algorithms/aStarSearch/a-star.js';
 
 let path = [];
+let memory = 0;
 puzzleStates.forEach(puzz => {
-    // path = new BreadthFirst().time(puzz);
-    // console.table(path);
+    path = new BreadthFirst().time(puzz);
+    console.table(path);
 
-    // path = AStar.search(puzz, goalState);
-    // console.table(path);
+
+    path = AStar.search(puzz, goalState);
+    console.log(`Memory Used: ${window.performance.memory.usedJSHeapSize} byte`);
+    memory += window.performance.memory.usedJSHeapSize
+    console.log(path);
 });
 
 path.length === 0 && alert('Please uncomment code in src/main.js to perform the searches :)');
